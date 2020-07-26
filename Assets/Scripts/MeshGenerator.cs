241b05544507a4d3e124af060f9a2f8b650878d4
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-07-26 16:55:15 +0900

    Mesh生成部をinterfaceに逃がす

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -2,56 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
-public class MeshGenerator : MonoBehaviour
+public class MeshGenerator : SerializedMonoBehaviour
 {
     [SerializeField] MeshFilter meshFilterPrefab;
+    [SerializeField] IGenerateMesh generateMesh;
 
     void Start()
     {
         MeshFilter meshFilter = Instantiate(meshFilterPrefab);
-        meshFilter.mesh = GenerateQuad();
-    }
-
-    Mesh GenerateQuad(){
-
-        Mesh mesh = new Mesh();
-
-        Vector3[] verts = new Vector3[]{
-            new Vector3(-1, 1),
-            new Vector3(1, 1),
-            new Vector3(-1, -1),
-            new Vector3(1, -1)
-        };
-
-        Vector2[] uvs = new Vector2[]{
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1)
-        };
-
-        Vector3[] normals = new Vector3[]{
-            new Vector3(0, 0, -1),
-            new Vector3(0, 0, -1),
-            new Vector3(0, 0, -1),
-            new Vector3(0, 0, -1)
-        };
-
-        int[] triangles = new int[]{
-            0, 1, 3,
-            0, 3, 2
-        };
-
-        mesh.vertices = verts;
-        mesh.uv = uvs;
-        mesh.normals = normals;
-        mesh.triangles = triangles;
-
-        //mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-
-        return mesh;
+        meshFilter.mesh = generateMesh.Generate();
     }
 
 
---
+++ Assets/Scripts/QuadGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MeshGenerator/Quad", fileName = "QuadGenerator", order = 0)]
+public class QuadGenerator : ScriptableObject, IGenerateMesh
+{
+    [SerializeField] float size = 1;
+
+    public Mesh Generate(){
+        Mesh mesh = new Mesh();
+
+        float size_2 = size / 2;
+
+        Vector3[] verts = new Vector3[]{
+            new Vector3(-size_2,  size_2),
+            new Vector3( size_2,  size_2),
+            new Vector3(-size_2, -size_2),
+            new Vector3( size_2, -size_2)
+        };
+
+        Vector2[] uvs = new Vector2[]{
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(1, 1),
+            new Vector2(0, 1)
+        };
+
+        Vector3[] normals = new Vector3[]{
+            new Vector3(0, 0, -1),
+            new Vector3(0, 0, -1),
+            new Vector3(0, 0, -1),
+            new Vector3(0, 0, -1)
+        };
+
+        int[] triangles = new int[]{
+            0, 1, 3,
+            0, 3, 2
+        };
+
+        mesh.vertices = verts;
+        mesh.uv = uvs;
+        mesh.normals = normals;
+        mesh.triangles = triangles;
+
+        //mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+}
