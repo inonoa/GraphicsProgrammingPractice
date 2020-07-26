231bf5badf773b4714995151183c531a0ab34e59
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-07-26 19:44:44 +0900

    Catmull-Rom曲線

---
+++ Assets/Scripts/CatmullRomCurve.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatmullRomCurve
+{
+    readonly IReadOnlyList<Spline> splines;
+
+    public CatmullRomCurve(float[] points){
+        Spline[] splines = new Spline[points.Length - 1];
+        for(int i = 0; i < points.Length - 1; i++){
+            splines[i] = new Spline(
+                points[i],
+                points[i+1],
+                i == 0 ? points[i+1] - points[i] : (points[i+1] - points[i-1]) / 2,
+                i == points.Length - 2 ? points[i] - points[i-1] : (points[i+2] - points[i]) / 2
+            );
+        }
+        this.splines = splines;
+    }
+
+    public float Calc(float t){
+        int index = Mathf.FloorToInt(t * splines.Count);
+        float t_in_spline = t * splines.Count % 1;
+        return splines[index].Calc(t_in_spline);
+    }
+}
---
+++ Assets/Scripts/Spline.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spline
+{
+    readonly float a, b, c, d;
+    public float Calc(float t){
+        return a * t*t*t + b * t*t + c * t + d;
+    }
+
+    public Spline(float p0, float p1, float v0, float v1){
+        a =  2 * p0 - 2 * p1 +     v0 + v1;
+        b = -3 * p0 + 3 * p1 - 2 * v0 - v1;
+        c = v0;
+        d = p0;
+    }
+}
