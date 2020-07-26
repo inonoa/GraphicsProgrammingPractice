9c8a9fa11bd248c59adad06bfbf1110179cdfb5d
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-07-26 19:58:35 +0900

    動かしてみたら動いた

--- Assets/Scripts/SplineTest.cs
+++ Assets/Scripts/SplineTest.cs
@@ -2,13 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using Sirenix.OdinInspector;
 
 public class SplineTest : MonoBehaviour
 {
+    [SerializeField] Vector2[] points;
 
     void Start()
     {
+
+    }
+
+    [Button]
+    void Move(){
         
+        var curveX = new CatmullRomCurve(points.Select(vec => vec.x).ToArray());
+        var curveY = new CatmullRomCurve(points.Select(vec => vec.y).ToArray());
+
+        StartCoroutine(Move(curveX, curveY));
+    }
+
+    IEnumerator Move(CatmullRomCurve curveX, CatmullRomCurve curveY){
+        float time = 0;
+        transform.position = new Vector3(curveX.Calc(0), curveY.Calc(0), 0);
+        yield return null;
+        while((time += Time.deltaTime) <= 10){
+            transform.position = new Vector3(curveX.Calc(time / 10), curveY.Calc(time / 10), 0);
+            yield return null;
+        }
     }
 
 
